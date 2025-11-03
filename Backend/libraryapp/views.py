224d94beade77c5b2984d4f5fee504412bd6e545
AmanRaj1813 <amanrajkharwat@gmail.com>
2025-11-03 12:24:08 +0530

    after borrow view

--- Backend/libraryapp/views.py
+++ Backend/libraryapp/views.py
@@ -4,6 +4,7 @@ from .models import User,Book, BorrowRecord,Category
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .utils import send_due_notification
 from django.utils import timezone
+from .serializers import UserSerializer,BookSerializer,BorrowRecordSerializer, CategorySerializer
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -19,3 +20,16 @@ class BorrowRecordViewSet(viewsets.ModelViewSet):
     queryset = BorrowRecord.objects.all()
     serializer_class = BorrowRecordSerializer
     permission_classes = [IsAuthenticated]
+    
+    def check_due_books(self):
+        records = BorrowRecord.objects.filter(due_date__lte=timezone.now())
+        for record in records:
+            if not record.return_date:
+                user_email = record.user.email
+                book_title = record.book.title
+                due_date = record.due_date.strftime('%Y-%m-%d')
+                send_due_notification(user_email, book_title, due_date)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
