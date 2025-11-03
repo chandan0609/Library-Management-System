cac9f891fff9e2d77e61000a896d80e9ca7bd7fa
AmanRaj1813 <amanrajkharwat@gmail.com>
2025-11-03 12:44:15 +0530

    6th commit after make migration

--- Backend/libraryapp/serializers.py
+++ Backend/libraryapp/serializers.py
@@ -5,17 +5,17 @@ from .models import User, Book, BorrowRecord, Category
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        field = ['id','username','email','password','role']
+        fields = ['id','username','email','password','role']
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        field = ['id','title','author','category','ISBN','status']
+        fields = ['id','title','author','category','ISBN','status']
 
 class BorrowRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = BorrowRecord
-        field = ['id','user','book','borrow_date','due_date','return_date']
+        fields = ['id','user','book','borrow_date','due_date','return_date']
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
