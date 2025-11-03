73faa424d425f84431cfa0f0d88d9635dea4b185
chandan0609 <chandanmohapatra0623@gmail.com>
2025-11-03 11:35:41 +0530

    third commit

--- Backend/libraryapp/views.py
+++ Backend/libraryapp/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 from .models import User,Book, BookRecord,Category
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .utils import send_due_notification
+from django.utils import timezone
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class BorrowRecordViewSet(viewsets.ModelViewSet):
+    queryset = BorrowRecord.objects.all()
+    serializer_class = BorrowRecordSerializer
+    permission_classes = [IsAuthenticated]
